8f6999115f41f87dc0bb0b6994e5ae43fe35d94b
ssyan110 <ssyan110@gmail.com>
2025-12-09 16:24:31 +0700

    Fix: import tailwind css styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css' // <--- 關鍵就是這一行！沒這行，Tailwind 就不會運作
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // 這行最重要，確保它能掃描到 src 裡的所有程式碼
   ],
   theme: {
     extend: {},
